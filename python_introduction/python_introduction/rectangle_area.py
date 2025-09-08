2c5b99c8da567b75ae06b6e9ddcb8b936c2133b0
Jamlic <150439323+Jamlic-1@users.noreply.github.com>
2025-09-08 18:27:38 +0300

    Delete python_introduction directory

--- python_introduction/python_introduction/rectangle_area.py
+++
@@ -1,5 +0,0 @@
-
-length = 10  # with a value representing the length of the rectangle.
-width = 5  # with a value representing the width of the rectangle.
-area = length * width
-print("The area of the rectangle is:", area)
--- python_introduction/simple_interest.py
+++
@@ -1,7 +0,0 @@
-principal = 1000# with a value of 1000 (representing $1000),
-rate = 0.05# with a value of 0.05 (representing 5% annual interest rate),
-time = 3# with a value of 3 (representing 3 years).
-
-interest = principal * rate* time
-
-print('The simple interest is:',interest)
