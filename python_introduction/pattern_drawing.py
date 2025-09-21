a757b43d31e8d8ca879ca98ac2004f8c2c04f471
Jamlic <150439323+Jamlic-1@users.noreply.github.com>
2025-09-21 08:59:49 +0300

    Create pattern_drawing.py

---
+++ python_introduction/pattern_drawing.py
@@ -0,0 +1,11 @@
+# pattern_drawing.py
+
+# Prompt user for pattern size
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()  # move to the next line
+    row += 1
